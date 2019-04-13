046ead8d56dc8cdf5b45c9a5eccfa76ce7523ac9
Danil Sorokin <d.sorokin.msk@icloud.com>
2019-04-13 22:09:36 +0300

    configuration of dev and prod modes

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,44 +1,51 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const WebpackShellPlugin = require('webpack-shell-plugin');
-
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const WebpackShellPlugin = require("webpack-shell-plugin");
 
 module.exports = (env, argv) => ({
-    entry: {
-        server: './src/entry-points/server.js',
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/',
-        filename: '[name].js'
-    },
-    watch: argv.mode === 'development',
-    target: 'node',
-    node: {
-        // Need this when working with express, otherwise the build fails
-        __dirname: false,   // if you don't put this is, __dirname
-        __filename: false,  // and __filename return blank or /
-    },
-    externals: [nodeExternals()], // Need this to avoid error when working with Express
-    resolve: {
-        alias: {
-            Api: path.resolve(__dirname, 'src/api'),
+  entry: {
+    server: "./src/entry-points/server.js"
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js"
+  },
+  watch: argv.mode === "development",
+  target: "node",
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false // and __filename return blank or /
+  },
+  externals: [nodeExternals()], // Need this to avoid error when working with Express
+  resolve: {
+    alias: {
+      server: path.resolve(__dirname, "src/server"),
+      Framework: path.resolve(__dirname, "src/framework"),
+      Domains: path.resolve(__dirname, "src/domains")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+            plugins: ["@babel/plugin-syntax-dynamic-import"]
+          }
         }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                },
-            },
-        ],
-    },
-    plugins: [
-        new WebpackShellPlugin({
-            onBuildEnd: [argv.mode === 'production' ? 'echo Server built!' : 'yarn start:dev'],
-        })
-    ],
+      }
+    ]
+  },
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildEnd: [
+        argv.mode === "production" ? "echo Server built!" : "yarn start:dev"
+      ]
+    })
+  ]
 });
